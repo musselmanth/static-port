0a64f550d02ac704db2beca38e39658c40fdde12
Thomas Musselman <tmusselman@gmail.com>
2023-04-22 08:04:46 -0600

    adjust nav bar spacing

--- scroll.js
+++ scroll.js
@@ -1,15 +1,19 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementsByClassName("social-buttons")[0].style.flexDirection = "row";
-    document.getElementsByClassName("headshot")[0].style.width = "50px";
-    document.getElementsByClassName("headshot")[0].style.height = "50px";
-    document.getElementById("role").style.display = "none";
+    document.getElementsByClassName("headshot")[0].style.width = "45px";
+    document.getElementsByClassName("headshot")[0].style.height = "45px";
+    document.getElementsByClassName("role")[0].style.display = "none";
+    document.getElementsByClassName("nav-bar")[0].style.padding = "5px 10px"
+    document.getElementsByClassName("nav-bar")[0].style.width = "calc(100% - 20px)"
   } else {
     document.getElementsByClassName("social-buttons")[0].style.flexDirection = "column";
-    document.getElementById("role").style.display = "inline";
+    document.getElementsByClassName("role")[0].style.display = "inline";
     document.getElementsByClassName("headshot")[0].style.width = "90px";
     document.getElementsByClassName("headshot")[0].style.height = "90px";
+    document.getElementsByClassName("nav-bar")[0].style.padding = "10px 20px"
+    document.getElementsByClassName("nav-bar")[0].style.width = "calc(100% - 40px)"
   }
 }
